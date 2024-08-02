0f205b1856930b6180e4abe0299bd0cc59099ea9
sci-code-711 <lloydhuw98@gmail.com>
2024-08-02 16:46:17 +0100

    Add a connector to output to mongo, And update the csv reader to read in header rows

--- deadrec/connectors.py
+++ deadrec/connectors.py
@@ -1,5 +1,7 @@
 from multiprocessing import Queue
 from .runners import Runner, TerminateSignal
+from pymongo import MongoClient
+from datetime import datetime, timedelta
 
 class Connector(Runner):
     pass
@@ -17,16 +19,24 @@ class OutputConnector(Connector):
 
 
 class FromCSV(IngestConnector):
-    def __init__(self, file_handle: str, sep: str = ","):
+    def __init__(self, file_handle: str, sep: str = ",", header=True):
         self.file_handle = file_handle
         self.delimiter = sep
+        self.header = header
         super().__init__()
 
     def run(self):
         with open(self.file_handle) as csv_file:
+
+            if self.header:
+                headers = next(csv_file).rstrip().split(self.delimiter)
+
             for line in csv_file:
                 row = line.rstrip().split(self.delimiter)
-                self.output_stream.put(row)
+                processed_row = {
+                    key: value for key, value in zip(headers, row)
+                }
+                self.output_stream.put(processed_row)
 
         print(f"Completed reading CSV file")
         self.output_stream.put(TerminateSignal(True, None))
@@ -51,3 +61,36 @@ class ToCSV(OutputConnector):
                 csv_file.write(parsed_row + "\n")
 
         print(f"Completed CSV file write")
+
+
+class ToMongo(OutputConnector):
+    def __init__(
+            self,
+            url: str,
+            database: str,
+            collection: str,
+            mapper: callable
+        ):
+        self.url = url
+        self.database_name = database
+        self.collection_name = collection
+        self.mapper = mapper
+        super().__init__()
+
+    def run(self):
+        self.mongodb_client = MongoClient(self.url)
+
+        self.collection = self.mongodb_client[
+            self.database_name
+        ].get_collection(
+            self.collection_name
+        )
+
+        while True:
+            item = self.input_stream.get()
+            if isinstance(item, TerminateSignal):
+                break
+
+            document = self.mapper(item)
+
+            self.collection.insert_one(document)
